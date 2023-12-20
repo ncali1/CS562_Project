16b13d418f19c17543b73de29c5313fc0d02f08e
Egnifluro <126028000+Egnifluro@users.noreply.github.com>
2023-12-20 17:34:53 -0500

    Updated generator.py

--- basecode/generator.py
+++ basecode/generator.py
@@ -1,5 +1,24 @@
 import subprocess
+import sys
+import copy
 
+# Search for a given “group by” attrib. value(s) in mf_struct
+def lookup(cur_row, V, NUM_OF_ENTRIES, mf_struct):
+    for i in range(NUM_OF_ENTRIES):
+        numSame = len(V)
+        for attrib in V:
+            if mf_struct[i][attrib] == cur_row[attrib]:
+                numSame -= 1
+            if numSame == 0:
+                return i
+    return -1
+
+# Adds a new entry in mf_struct, corresponding to a newly found “group by” attrib. value
+def add(cur_row, V, NUM_OF_ENTRIES, mf_struct):
+    for attrib in V:
+        mf_struct[NUM_OF_ENTRIES][attrib] = cur_row[attrib]
+    NUM_OF_ENTRIES += 1
+    return NUM_OF_ENTRIES
 
 def main():
     """
@@ -7,13 +26,63 @@ def main():
     needed to run the query. That generated code should be saved to a 
     file (e.g. _generated.py) and then run.
     """
+    # Reading input
+    """
+    Usage:  python generator.py <.txt>
+    Ex:     See example.txt
+    """
+    if len(sys.argv) == 2: # From file
+        f = open(sys.argv[1], "r")
+        S = f.readline()
+        n = f.readline()
+        V = f.readline()
+        F_Vect = f.readline()
+        Pred_List = f.readline()
+        Having = f.readline()
+        S = S.strip("\n")
+        n = n.strip("\n")
+        V = V.strip("\n")
+        F_Vect = F_Vect.strip("\n")
+        Pred_List = Pred_List.strip("\n")
+        Having = Having.strip("\n")
+    """
+    Usage:  python generator.py <S> <n> <V> <F_Vect> <Pred_List> <Having>
+    Ex:     python generator.py "cust, count_1_quant, sum_2_quant, max_3_quant" 3 cust "count_1_quant, sum_2_quant, avg_2_quant, max_3_quant" "1.state = 'NY'; 2.state = 'NJ'; 3.state = 'CT'" None
+    """
+    if len(sys.argv) == 7: # From command line
+        S = sys.argv[1] #  1. S - projected columns / expressions
+        n = sys.argv[2] #  2. n - number of grouping variables
+        V = sys.argv[3] #  3. V - grouping attributes
+        F_Vect = sys.argv[4] #  4. F-VECT – vector of aggregate functions
+        Pred_List = sys.argv[5] # 5. PRED-LIST – list of predicates for grouping
+        Having = sys.argv[6] # 6. HAVING
+    # Process Phi expression arguments
+    S = S.split(", ")
+    n = int(n)
+    V = V.split(", ")
+    F_Vect = F_Vect.split(", ")
+    Pred_List = Pred_List.split("; ")
+    # Having unsure about the format of the input
+    # Generate the code for mf_struct
+    base_struct = {}
+    for attrib in V:
+        base_struct[attrib] = None
+    for aggre in F_Vect:
+        base_struct[aggre] = 0
+    mf_struct = []
+    for i in range(500):
+        mf_struct.append(copy.deepcopy(base_struct))
+    NUM_OF_ENTRIES = 0
+    # Populate mf-struct with distinct values of grouping attribute
+
+    '''
 
     ############################################################################################
     # READ INPUT (READING FROM FILE)
-
+    
 
     # READ INPUT (INTERACTIVELY FROM USER)
-
+        
 
     # CREATING MF STRUCT
 
@@ -50,17 +119,25 @@ def main():
     ## AVERAGE (FOR LOOP) (RELIES ON COUNT AND SUM)
     # similar to max?
 
+    ''' 
     ####### END OF QUERY PROCESSING ####### 
 
     # INJECT FOR LOOPS INTO body VARIABLE
 
 
     ############################################################################################
-
-    body = """
+    """
     for row in cur:
         if row['quant'] > 10:
             _global.append(row)
+    """
+    body = f"""
+    S = {S}
+    n = {n}
+    V = {V}
+    F_Vect = {F_Vect}
+    Pred_List = {Pred_List}
+    Having = {Having}
     """
 
     # Note: The f allows formatting with variables.
